2627b317156700e8af95a60678b322882b2ea529
João Eduardo <joao22dudu@gmail.com>
2023-09-26 21:05:57 -0300

    terminar

--- galloflix/src/app/pages/home/home.component.ts
+++ galloflix/src/app/pages/home/home.component.ts
@@ -3,30 +3,35 @@ import { MovieApiService } from 'src/app/services/movie-api.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html', styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   constructor(private service: MovieApiService) { }
   bannerResult: any = [];
   trendingMovieResult: any = [];
+ 
 
   ngOnInit(): void {
     this.bannerData();
     this.trendingData();
+    
   }
 
   // Banner Data 
   bannerData() {
     this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerResult#');
       this.bannerResult = result.results;
     });
   }
-// Trending Data 
+
   trendingData() {
     this.service.trendingMovieApiData().subscribe((result) => {
-      this.trendingMovieResult = result.results;
-   });
+      this.trendingMovieResult = result.results
+    })
   }
+
   }
   
 
--- galloflix/src/app/pages/movie-details/movie-details.component.ts
+++ galloflix/src/app/pages/movie-details/movie-details.component.ts
@@ -4,19 +4,21 @@ import { MovieApiService } from 'src/app/services/movie-api.service';
 
 @Component({
   selector: 'app-movie-details',
-  templateUrl: './movie-details.component.html', styleUrls: ['./movie-details.component.css']
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent {
-  constructor(private service: MovieApiService, private router: ActivatedRoute) { }
+  constructor(private service:MovieApiService, private router:ActivatedRoute) { }
    movieResult: any;
 
    ngOnInit(): void {
-    let id = this.router.snapshot.paramMap.get('id'); this.getMovie(id);
+    let id = this.router.snapshot.paramMap.get('id');
+    this.getMovie(id);
   }
 
-  getMovie(id: any) {
-    this.service.movieDetails(id).subscribe((result) => {
-     console.log(result, 'movieDetails#');
+  getMovie(id:any) {
+    this.service.movieDetails(id).subscribe((result)=>{
+     //console.log(result, 'movieDetails#');
      this.movieResult = result;
     });
   }
